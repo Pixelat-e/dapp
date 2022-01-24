ae35a71f339f7729631a8b7962a92ea3d729d2a4
Mayank Panjiyara <69447074+mayankp291@users.noreply.github.com>
2022-01-24 23:29:08 +0800

    Image works on final page

--- src/views/Final.js
+++ src/views/Final.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React,{useState} from "react";
 
 import Navbar from "components/Navbars/IndexNavbar.js";
 import Footer from "components/Footers/Footer.js";
 import Raccoon1 from "assets/raccoon_1.png"
+import BoardManager from "../components/PixelCanvas/BoardManager";
 import Web3 from "web3";
 import { useLocation } from 'react-router-dom'
 
 
 export default function Final({}) {
   const location = useLocation()
+    let bm = new BoardManager();
+    const [imgUrl, setImgUrl] = useState(bm.getLocalImg());
 
-  const { name, cname, desc } = location.state;
-
+  //const { name, cname, desc } = location.state;
+  const name = "asdhjkasjhd"
+  const cname = "adasd"
+  const desc = "asdad"
   return (
     <>
       <Navbar transparent />
@@ -50,62 +55,44 @@ export default function Final({}) {
           </div>
         </section>
         <section className="relative py-16 bg-blueGray-200">
-
-
           <div className="container mx-auto px-4">
             <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
               <div className="px-6">
-
-              
-                {/* <div className="flex flex-wrap justify-center">
-                  <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
-                    <div className="relative">
-                      <img
-                        alt="..."
-                        src={require("assets/img/team-2-800x800.jpg").default}
-                        className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"
-                      />
-                    </div>
-                  </div>
-                </div> */}
-
-
-
                 <div className="flex flex-wrap justify-center py-40">
                   <div className="w-full lg:w-6/12 px-4 lg:order-1 flex justify-center">
                     <div className="relative">
                       <img
                         alt="..."
-                        src={Raccoon1}
+                        src={bm.getLocalImg()}
                         className="shadow-xl h-auto align-middle border-none absolute  lg:-ml-16 max-w-210-px mt-12"
                       />
                     </div>
                   </div>
                   <div className="w-full lg:w-6/12 px-4 lg:order-2">
-                      <div className=" text-center">
-                        <h3 className="text-4xl font-bold leading-normal mb-2 text-blueGray-700 mb-2">
-                          Congratulations, your NFT has been minted!
-                        </h3>
-                        <div className="text-left">
-                          <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
-                            Name: {name}
-                          </div>
-                          <div className="mb-2 text-blueGray-600">
-                            Community: {cname}
-                          </div>
-                          <div className="mb-2 text-blueGray-600">
-                            Description: {desc}
-                          </div>
-                          {/* <div className="mb-2 text-blueGray-600">
-                            Contact Address: {}
-                          </div>
-                          <div className="mb-2 text-blueGray-600">
-                            Token ID: {}
-                          </div>
-                          <div className="mb-2 text-blueGray-600">
-                            Chain: {}
-                          </div> */}
+                    <div className=" text-center">
+                      <h3 className="text-4xl font-bold leading-normal mb-2 text-blueGray-700 mb-2">
+                        Congratulations, your NFT has been minted!
+                      </h3>
+                      <div className="text-left">
+                        <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
+                          Name: {name}
                         </div>
+                        <div className="mb-2 text-blueGray-600">
+                          Community: {cname}
+                        </div>
+                        <div className="mb-2 text-blueGray-600">
+                          Description: {desc}
+                        </div>
+                        <div className="mb-2 text-blueGray-600">
+                          IPFS Link: {desc}
+                        </div>
+                        <div className="mb-2 text-blueGray-600">
+                          Minted Token ID: {desc}
+                        </div>
+                        <div className="text-xl font-bold mb-2 text-blueGray-600">
+                          The NFT has been added to your OpenSeas account associated with your MetaMask ID.
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
