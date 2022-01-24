f9f0f2e471a08ca756a4e09668943f7b8f1fec88
Mayank Panjiyara <69447074+mayankp291@users.noreply.github.com>
2022-01-24 14:39:32 +0800

    Add moralis config details

---
+++ src/config.js
@@ -0,0 +1,5 @@
+
+//moralis config
+const SERVER_URL = "https://izaeqmus36qm.usemoralis.com:2053/server";
+const APP_ID = "RcQ2ZZxeW4ZUFDYFK9hIi6QZHYE3iBl6M2HgjtU8";
+const MASTER_KEY = "gahCQC0brERXzGssFIej2dkW2bv8QsYUaCAauni5";
